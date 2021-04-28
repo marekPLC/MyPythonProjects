2bd29c5f8f463844c89a0fe3e25ae4d7fb75364b
marekPLC <75478156+marekPLC@users.noreply.github.com>
2021-04-28 22:36:28 +0200

    RockPaperScissors

---
+++ rockpaperscissors.py
@@ -0,0 +1,20 @@
+import random
+
+
+list = ['rock', 'paper', 'scissors']
+user_input = str(input('Type you guess \n'))
+guessed = False
+
+
+def nahoda(var):
+    x = random.choice(list)
+    print(x)
+    if x == var:
+        guessed = True
+        return print(f'Good choice, good game! It was {x}'), guessed
+    else:
+        guessed = False
+        return print(f'Try again :(, the word was {x}'), guessed
+
+
+print(nahoda(user_input))
