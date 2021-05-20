c2f6b30efd8378c5810dfcff33b9dc1422e4f7ee
Marek Šteinc <marek@steinc.cz>
2021-05-20 12:22:03 +0200

    Update reversed.py

--- reversed.py
+++ reversed.py
@@ -1,6 +1,10 @@
 user_in = input('Type what you want to reverse\n')
+word = 'lmaoxD'
 splitted = user_in.split()
-print(splitted)
 
-for i in reversed(splitted):
+for j in reversed(splitted): #reverse list
+    print(j)
+
+
+for i in reversed(user_in):#reverse word
     print(i)
